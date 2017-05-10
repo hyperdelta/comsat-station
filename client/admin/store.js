bfef5601766efc81ffd280e554153f331da6f6fd
yujoonc <yujooncho@gmail.com>
2017-05-11 08:32:56 +0900

    rethink - io sample connection

--- client/admin/store.js
+++ client/admin/store.js
@@ -1,12 +1,11 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 
 import { router5Middleware, router5Reducer } from 'redux-router5';
-import logger from 'redux-logger';
 import breadcrumb from './reducers/breadcrumb';
 import func from './reducers/func';
 
 export default function configureStore(router, initialState = {}) {
-  const createStoreWithMiddleware = applyMiddleware(router5Middleware(router), logger())(createStore);
+  const createStoreWithMiddleware = applyMiddleware(router5Middleware(router))(createStore);
   const store = createStoreWithMiddleware(combineReducers({
     router: router5Reducer,
     breadcrumb,
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -12,8 +13,61 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 app.use('/', router);
 
-
-var port = process.argv[2] || 8080;
-app.listen(port, function () {
-  console.log('Example app listening on '+port)
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../../client/admin/admin.html'));
 });
+
+var server = require('http').Server(app);
+
+// Socket.io
+var io = require('socket.io')(server);
+
+// Rethinkdb
+var r = require('rethinkdb');
+
+// Socket.io changefeed events
+var changefeedSocketEvents = require('./changefeedSocketEvents.js');
+
+//rethink - io connection setting
+r.connect({ db: 'comsat_station' })
+    .then(function(connection) {
+      console.log(connection);
+        io.on('connection', function (socket) {
+
+            // insert new todos
+            socket.on('todo:client:insert', function(todo) {
+                r.table('cs_test').insert(todo).run(connection);
+            });
+
+            // update todo
+            socket.on('todo:client:update', function(todo) {
+                var id = todo.id;
+                delete todo.id;
+                r.table('cs_test').get(id).update(todo).run(connection);
+            });
+
+            // delete todo
+            socket.on('todo:client:delete', function(todo) {
+                var id = todo.id;
+                delete todo.id;
+                r.table('cs_test').get(id).delete().run(connection);
+            });
+
+            // emit events for changes to todos
+             r.table('cs_test').changes({ includeInitial: true, squash: true }).run(connection)
+                 .then(changefeedSocketEvents(socket, 'todo'));
+        });
+        //server
+        var port = process.argv[2] || 3000;
+
+        server.listen(port, function () {
+            console.log('Example app listening on '+port)
+        });
+    })
+    .error(function(error) {
+        console.log('Error connecting to RethinkDB!');
+        console.log(error);
+    });
+
+
+
